dcbc5f413e9d7d575de54df96767071fd18b7921
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2022-06-03 15:09:39 +0100

    app shows notifications

--- lib/controller/notification_contrller.dart
+++ lib/controller/notification_contrller.dart
@@ -1,23 +1,97 @@
-import 'package:fenwicks_pub/controller/auth_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fenwicks_pub/view/constant/color.dart';
+import 'package:fenwicks_pub/view/orders/order_details.dart';
+import 'package:fenwicks_pub/view/widget/error_card.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
 class NotificationController extends GetxController {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  final AuthController authController = Get.put(AuthController());
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  final CollectionReference<Map<String, dynamic>> _ref = FirebaseFirestore.instance.collection("orders");
+
+  final AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.high,
+  );
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<Map<String, dynamic>> getOrder(String id) async {
+    DocumentSnapshot<Map<String, dynamic>>? doc;
+    try {
+      doc = await _ref.doc(id).get();
+    } on FirebaseException catch (e) {
+      Get.showSnackbar(errorCard(e.message!));
+    }
+    return {
+      "data": doc!.data()!,
+      "id": doc.id
+    };
+  }
 
   @override
   void onInit() async {
-    // messaging.onTokenRefresh.listen((token) async {
-    //   await authController.updateUserData({
-    //     "token": token
-    //   });
-    // });
-    // final token = await messaging.getToken();
+    await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
 
-    // await authController.updateUserData({
-    //   "token": token
-    // });
+    //
+    var initialzationSettingsAndroid = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings = InitializationSettings(android: initialzationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    //
+    await _messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    //
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              color: kPrimaryColor,
+              icon: "@mipmap/ic_launcher",
+            ),
+            iOS: const IOSNotificationDetails(),
+          ),
+        );
+      }
+    });
+
+    //
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        Get.showSnackbar(
+          GetSnackBar(
+            title: notification.title,
+            message: notification.body,
+            snackPosition: SnackPosition.TOP,
+            duration: const Duration(seconds: 3),
+          ),
+        );
+      }
+    });
+
+    //
     super.onInit();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:fenwicks_pub/controller/binding.dart';
+import 'package:fenwicks_pub/firebase_options.dart';
 import 'package:fenwicks_pub/routes/routes.dart';
 import 'package:fenwicks_pub/view/constant/app_styling.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   Stripe.publishableKey = "pk_test_51L2iF5BCBx7eZ7I9hZ9P0kxUNABlNR9agT099DBfLkQb9woXnkDllth7kMOjMlfDuvNBZPulYHc9iejgNtpKeVhN00UtdY7Obg";
-  // Stripe.merchantIdentifier = "Fenwick's Pub";
   return runApp(const MyApp());
 }
 
